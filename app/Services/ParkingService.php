8cdd1ccf460e5c098727ed84ded2a826c3f80460
misprogrammer2021 <asma.normazlan@gmail.com>
2023-10-09 20:43:15 +0800

    feat(parking-fee): add test case for parking fee

--- app/Services/ParkingService.php
+++ app/Services/ParkingService.php
@@ -1,21 +1,72 @@
 <?php
 
 namespace App\Services;
-
+use Carbon\Carbon;
 /**
  * Class ParkingService.
  */
 class ParkingService
 {
-    public function calculateParkingFee($hoursParked)
+    
+    public function calculateParkingFee($startTime, $endTime)
     {
-        $baseFee = 5;
-        $additionalHourFee = 2;
+       $weekDay = $this->weekdayParkingFee($startTime, $endTime);
+       $weeKend = $this->weekendParkingFee($startTime, $endTime);
 
-        if ($hoursParked <= 1) {
-            return $baseFee;
-        }
+       $startDay = Carbon::parse($startTime)->isWeekday();
 
-        return $baseFee + ($additionalHourFee * ($hoursParked - 1));
+       if ($startDay)  {
+            return $weekDay;
+       } else {
+            return $weeKend;
+       } 
     }
+
+    private function weekdayParkingFee($startTime, $endTime) {
+        $start = Carbon::parse($startTime);
+        $end = Carbon::parse($endTime);
+        $hoursParked = $end->diffInHours($start);
+        $minutesParked = $end->diffInMinutes($start);
+        $more24hoursParked = $start->floatDiffInDays($end);
+        $subsequentHour = $hoursParked - 3;
+        $additionalHour = ($subsequentHour * 1.50) + 3;
+        $totalPrice = $additionalHour;
+        $totalPriceMore24Hours = 20 + 3 + 7.50;
+
+        if ($minutesParked <= 15) {
+            return 0.00;
+        } else if ($hoursParked > 3 && $hoursParked < 24)  {
+            return $totalPrice;
+        } else if ($more24hoursParked > 24) {
+            return $totalPriceMore24Hours;
+        } 
+        else {
+            return 3.0;
+        }
+    }
+
+    private function weekendParkingFee($startTime, $endTime) {
+        $start = Carbon::parse($startTime);
+        $end = Carbon::parse($endTime);
+        $hoursParked = $end->diffInHours($start);
+        $minutesParked = $end->diffInMinutes($start);
+        $more24hoursParked = $start->floatDiffInDays($end);
+        $subsequentHour = $hoursParked - 5.00;
+        $additionalHour = ($subsequentHour * 2.00) + 5.00;
+        $totalPrice = $additionalHour;
+        $totalPriceMore24Hours = 40 + 5 + 7.50;
+
+        if ($minutesParked <= 15) {
+            return 0.00;
+        } else if ($hoursParked > 3 && $hoursParked < 24) {
+            return $totalPrice;
+        } 
+        else if ($more24hoursParked > 24) {
+            return $totalPriceMore24Hours;
+        } 
+        else {
+            return 5.00;
+        }
+    }
+    
 }
--- tests/Feature/ParkingFeeTest.php
+++ tests/Feature/ParkingFeeTest.php
@@ -5,106 +5,134 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-// use App\Services\ParkingService;
-use App\Http\Controllers\ParkingController;
 use Carbon\Carbon;
 
 class ParkingFeeTest extends TestCase
 {
-    public function testCalculateParkingFee()
+    public function test_calculate_parking_fee_below_15_minutes_on_weekday(): void
     {
-        $startTime = Carbon::now();
-        $endTime = Carbon::now()->addHours(1.5); 
-        $response = $this->postJson('/calculate-parking-fee', [
-            'start_time' => $startTime->toDateTimeString(),
-            'end_time' => $endTime->toDateTimeString(),
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-25 08:27:00';
+
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
         ]);
 
         $response->assertStatus(200)
-            ->assertJson(['fee' => 3]); 
+            ->assertJson(['fee' => 0.00]);
     }
 
-    // public function testCalculateParkingFee()
+    public function test_calculate_parking_fee_below_15_minutes_on_weekend(): void
+    {
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-25 08:27:00';
+
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(['fee' => 0.00]);
+    }
+
+    public function test_calculate_parking_fee_below_3_hours_on_weekday(): void
+    {
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-25 10:27:00';
+
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(['fee' => 3.00]);
+    }
+
+    public function test_calculate_parking_fee_below_3_hours_on_weekend(): void
+    {
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-25 10:27:00';
+
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(['fee' => 5.00]);
+    }
+
+    public function test_calculate_parking_fee_more_3_hours_on_weekday(): void
+    {
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-25 12:19:00';
+
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(['fee' => 4.50]);
+    }
+
+    public function test_calculate_parking_fee_more_3_hours_on_weekend(): void
+    {
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-25 12:19:00';
+
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(['fee' => 3.00]);
+    }
+
+    // public function testCalculateParkingFeeMore8hours()
     // {
-    //     // Arrange
-    //     $calculator = new ParkingController();
-    //     $entryTime = Carbon::parse('2023-10-06 08:00:00'); // Replace with your desired entry time
-    //     $exitTime = Carbon::parse('2023-10-06 17:30:00'); // Replace with your desired exit time
+    //     $startTime = '2021-10-25 08:16:00';
+    //     $endTime = '2021-10-25 16:27:00';
 
-    //     // Act
-    //     $fee = $calculator->calculateFee($entryTime, $exitTime);
+    //     $response = $this->post('/api/calculate-parking-fee', [
+    //         'start_time' => $startTime,
+    //         'end_time' => $endTime,
+    //     ]);
 
-    //     // Assert
-    //     // Replace the expectedFee with the expected parking fee for the given entry and exit times
-    //     $expectedFee = 12.50; // Replace with your expected fee
-    //     $this->assertEquals($expectedFee, $fee);
+    //     $response->assertStatus(200)
+    //         ->assertJson(['fee' => 10.50]);
     // }
 
-    // // Test case to calculate parking fee for one hour of parking.
-    // public function testCalculateParkingFeeForOneHour()
-    // {
-    //     $start = Carbon::parse('2023-10-06 10:00:00');
-    //     $end = Carbon::parse('2023-10-06 11:00:00');
-    //     $carbonNow = Carbon::parse('2023-10-06 11:15:00'); // Simulate "now" for the test
+    public function test_calculate_parking_fee_more_24_hours_on_weekday()
+    {
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-26 08:16:00';
 
-    //     $fee = calculateParkingFee($start, $end, $carbonNow);
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+        ]);
 
-    //     $this->assertEquals(5.00, $fee); // Adjust the expected fee as per your pricing logic
-    // }
+        $response->assertStatus(200)
+            ->assertJson(['fee' => 30.50]);
+    }
 
-    // public function testCalculateParkingFeePer1Hour()
-    // {
-    //     $parkingService = new ParkingService();
-    //     $fee = $parkingService->calculateParkingFee(1);
-    //     $in = Carbon::parse('2023-10-05 08:00:00');
-    //     $out = Carbon::parse('2023-10-05 09:00:00');
+    public function test_calculate_parking_fee_more_24_hours_on_weekend()
+    {
+        $startTime = '2021-10-25 08:16:00';
+        $endTime = '2021-10-26 08:16:00';
 
-    //     $fee = $parkingService($in, $out);
+        $response = $this->post('/api/calculate-parking-fee', [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+        ]);
 
-    //     $this->assertEquals(5.00, $fee);
-    // }
-    // public function testCalculateParkingFee()
-    // {
-    //     $controller = new ParkingController();
-    //     $in = '2023-10-05 08:00:00';
-    //     $out = '2023-10-05 14:30:00';
-
-    //     $fee = $controller->calculateParkingFee($in, $out);
-
-    //     $this->assertEquals(10.50, $fee);
-    // }
-    // public function testScenarioOne()
-    // {
-    //     $parkingService = new ParkingService();
-    //     $fee = $parkingService->calculateParkingFee(1);
-    //     $data = [
-    //         'reg_no' => 'SU 123 K',
-    //         'in' => '2021-10-25 08:16:00',
-    //         'out' => '2021-10-25 08:27:00',
-    //         'duration' => '11 minutes',
-    //         'amount_to_paid' => '$0.00',
-    //     ];
-
-    //     $response = $this->post('/calculate-parking-fee', $data);
-    //     $response->assertStatus(200);
-    //     $response->assertSee('Amount to paid: $0.00');
-    // }
-
-    // public function testScenarioTwo()
-    // {
-    //     $parkingService = new ParkingService();
-    //     $fee = $parkingService->calculateParkingFee(1);
-    //     $data = [
-    //         'reg_no' => 'SN 3453 G',
-    //         'in' => '2021-10-25 08:16:00',
-    //         'out' => '22021-10-25 12:19',
-    //         'duration' => '4 hours 3 minutes',
-    //         'amount_to_paid' => '$4.50',
-    //     ];
-
-    //     $response = $this->post('/calculate-parking-fee', $data);
-    //     $response->assertStatus(200);
-    //     $response->assertSee('Amount to paid: 4.50');
-    // }
+        $response->assertStatus(200)
+            ->assertJson(['fee' => 52.50]);
+    }
 }
 
